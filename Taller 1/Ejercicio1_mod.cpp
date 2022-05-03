84ac6d64c7c5ee5fae07c5b4c6605c0ef9b28d98
felimarod <felipemartinrod@gmail.com>
2022-05-02 21:32:21 -0500

    Se deja ejercicio 1 y se agrega el ejercicio 1 modificado

---
+++ Taller 1/Ejercicio1_mod.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void imprimirTamanio(string nombre, int a){
+	cout << "Tamanio de " << nombre <<" : " << sizeof(a) << endl;
+}
+void imprimirTamanio(string nombre, int* a){
+	cout << "Tamanio de " << nombre <<" : " << sizeof(a) << endl;
+}
+
+void proceso(int N){
+	bool aSumado = false;
+	int tamanio = 0;
+	
+	int i, j, *a;
+	
+	tamanio += sizeof(N) + sizeof(i) + sizeof(j) + sizeof(a);
+	//imprimirTamanio("N", N);
+	//imprimirTamanio("i", i);
+	//imprimirTamanio("j", j);
+	
+	
+	for(i=0;i<N;i++){
+		a = new int [N];
+		//if(!aSumado)
+			//imprimirTamanio("a", a);
+		for(j=0;j<N;j++){	
+			a[j] = j*i;
+			
+			if(!aSumado){
+				//imprimirTamanio("Elemento dentro de a", a[j]);
+				tamanio+=sizeof(a[j]);
+			}
+			//cout << a[j] << endl;
+			if(j==N-1){
+				aSumado = true;	
+			}
+		}
+		delete []a;
+	}
+	
+	cout << "Si N = "<< N << ", el tamanio es: " << tamanio << endl;
+}
+int main(){
+	proceso(50);
+	proceso(100);
+	proceso(150);
+	proceso(200);
+	proceso(1000);
+}
