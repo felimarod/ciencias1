87c4ba4ad3216dbb37f2e9e372ce45d5ed761b9e
felimarod <felipemartinrod@gmail.com>
2022-07-22 19:33:49 -0500

    fin colas de prioridad

--- 11.Arboles/12.ColasDePrioridad/ColasDePrioridad.cpp
+++ 11.Arboles/12.ColasDePrioridad/ColasDePrioridad.cpp
@@ -13,9 +13,8 @@ public:
   monticulo(int _tam) {
     Arr= new int[_tam + 1];
     PosAct = 1;
-    for (int i = 0; i < _tam; i++) {
+    for (int i = 0; i < _tam; i++)
       Arr[i] = 0;
-    }
     tam = _tam;
   };
   ~monticulo() { delete Arr; }
@@ -42,37 +41,36 @@ bool monticulo::insertar(int dato) {
   return true;
 }
 int monticulo::atender() {
-  int va; // Valor atendido
-  int uv; // Ultimo valor
+  int va; /* Valor atendido */
+  int uv; /* Ultimo valor */
   int auxPos = 1;
   if (Arr[1] == 0)
     return 0;
   else {
-    va = Arr[1]; // Guarda el valor a retornar
-    Arr[1] = 0; // Vuelve 0 el valor de la raíz
+    va = Arr[1]; /* Guarda el valor a retornar */
+    Arr[1] = 0; /* Vuelve 0 el valor de la raíz */
     if (PosAct == 0) PosAct = tam;
     else PosAct--;
-    uv = Arr[PosAct]; // Guarda el último valor
-    Arr[PosAct] = 0; // Vuelve 0 el último valor
-    do {
+    uv = Arr[PosAct]; /* Guarda el último valor */
+    Arr[PosAct] = 0; /* Libera el último espacio */
+    while(auxPos * 2 <= tam
+          && (uv < Arr[auxPos * 2] || uv < Arr[auxPos * 2 + 1])){
       if (uv < Arr[auxPos * 2]) {
         Arr[auxPos] = Arr[auxPos * 2];
         auxPos = auxPos * 2;
-        Arr[auxPos] = 0;
-      } else if (uv < Arr[auxPos * 2 + 1]) {
+      } else {
         Arr[auxPos] = Arr[auxPos * 2 + 1];
         auxPos = auxPos * 2 + 1;
-        Arr[auxPos] = 0;
       }
-    } while (auxPos * 2 + 1 <= tam && (uv < Arr[auxPos * 2] ||
-                                       uv < Arr[auxPos * 2 + 1]));
-    cout << endl;
+      Arr[auxPos] = 0;
+    }
     Arr[auxPos] = uv;
     return va;
   }
 }
 
 void monticulo::imprimirArreglo() {
+  cout << endl;
   for (int i = 1; i <= tam; i++)
     cout << "Posicion " << i << ":  \t" << Arr[i] << endl;
 }
